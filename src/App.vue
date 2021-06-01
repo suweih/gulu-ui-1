a2656bf583b6a01fe820bb4b5bb7cce0e2b66039
suweih <475464842@qq.com>
2021-06-01 15:35:08 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Su/>
+</template>
+
+<script>
+import Su from './components/Su.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Su: Su
+  }
+}
+</script>
---
+++ src/components/Su.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>这是我的第一个组件</div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style  scoped>
+
+</style>
