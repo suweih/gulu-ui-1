ccb7b9c577c2102d1499ef639104e3b571132337
suweih <475464842@qq.com>
2021-06-03 23:00:05 +0800

    优化切换标签

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,17 +1,22 @@
 <template>
   <h2>示例一</h2>
-  <Tabs selected="导航1">
+  <Tabs v-model:selected="x">
     <Tab title="导航1">内容1</Tab>
-    <Tab title="导航2">内容2</Tab>
+    <Tab title="导航22222">内容2</Tab>
   </Tabs>
 </template>
 
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue';
 import Tab from '../lib/Tab.vue';
+import {ref} from 'vue';
 
 export default {
-  components: {Tabs, Tab}
+  components: {Tabs, Tab},
+  setup() {
+    const x = ref('导航1');
+    return {x};
+  }
 };
 
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,29 +1,64 @@
 <template>
   <div class="gulu-tabs">
-    <div class="gulu-tabs-nav">
-      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{ t }}</div>
+    <div class="gulu-tabs-nav" ref="container">
+      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles"
+           :ref="el=>{if (t ===selected) selectedItem = el}"
+           @click="select(t)" :key="index"
+           :class="{selected: t === selected}">{{ t }}
+      </div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
-      <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+      <component class="gulu-tabs-content-item"
+                 v-for="(c,index) in defaults" :is="c" :key="index"
+                 :class="{selected : c.props.title === selected}"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue';
+import {computed, onMounted, onUpdated, ref} from 'vue';
 
 export default {
+  props: {
+    selected: {
+      type: String
+    },
+  },
   setup(props, context) {
+    const selectedItem = ref<HTMLDivElement>(null);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    const x = () => {
+      const {width} = selectedItem.value.getBoundingClientRect();
+      indicator.value.style.width = width + 'px';
+      const {left: left1} = container.value.getBoundingClientRect();
+      const {left: left2} = selectedItem.value.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + 'px';
+    };
+    onMounted(x);
+    onUpdated(x);
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
         throw new Error('Tabs 子标签名必须是Tab');
       }
     });
-    const titles = defaults.forEach((tag) => {
+    const current = computed(() => {
+      console.log('重新 return');
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
+    });
+    const titles = defaults.map((tag) => {
       return tag.props.title;
     });
-    return {defaults, titles};
+    const select = (title: string) => {
+      context.emit('update:selected', title);
+    };
+    return {defaults, titles, current, select, selectedItem, indicator, container};
   }
 };
 </script>
@@ -37,20 +72,44 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
+
     &-item {
       padding: 8px 0;
       margin: 0 16px;
       cursor: pointer;
+
+
       &:first-child {
         margin-left: 0;
       }
+
       &.selected {
         color: $blue;
       }
     }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
   }
+
   &-content {
     padding: 8px 0;
+
+    &-item {
+      display: none;
+
+      &.selected {
+        display: block;
+      }
+    }
   }
 }
 </style>
