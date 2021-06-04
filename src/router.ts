89535180fe2d8e11cc3a3be18d0d6af9ff0ed6dd
suweih <475464842@qq.com>
2021-06-04 14:51:18 +0800

    添加三个路由

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,9 @@ import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
 import DocDemo from './components/DocDemo.vue';
+import Intro from './views/Intro.vue';
+import Install from './views/Install.vue';
+import GetStarted from './views/GetStarted.vue';
 
 const history = createWebHashHistory();
 export const router = createRouter({
@@ -17,6 +20,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         {path: '', component: DocDemo},
+        {path: 'intro', component: Intro},
+        {path: 'install', component: Install},
+        {path: 'get-started', component: GetStarted},
         {path: 'switch', component: SwitchDemo},
         {path: 'button', component: ButtonDemo},
         {path: 'dialog', component: DialogDemo},
