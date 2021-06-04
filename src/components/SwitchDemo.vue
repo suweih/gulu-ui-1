8afd87fc9a6bacd3d3770263661c7e8ca6d8cff7
suweih <475464842@qq.com>
2021-06-04 23:31:32 +0800

    component小优化

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,42 +4,41 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch1Demo/>
+        <component :is="Switch1Demo"/>
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{Switch1Demo.__sourceCode}}</pre>
+        <pre>{{ Switch1Demo.__sourceCode }}</pre>
       </div>
     </div>
     <div class="demo">
       <h2>支持 disabled </h2>
       <div class="demo-component">
-        <Switch2Demo/>
+        <component :is="Switch2Demo"/>
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{Switch2Demo.__sourceCode}}</pre>
+        <pre>{{ Switch2Demo.__sourceCode }}</pre>
       </div>
     </div>
   </div>
 </template>
 <script>
-import Switch from "../lib/Switch.vue"
 import Button from "../lib/Button.vue"
-import Switch1Demo from './Switch1.demo.vue'
-import Switch2Demo from './Switch2.demo.vue'
+import Switch1Demo from "./Switch1.demo.vue"
+import Switch2Demo from "./Switch2.demo.vue"
 
 import {ref} from "vue"
 
 export default {
-  components: {Switch, Button, Switch1Demo, Switch2Demo},
+  components: {Button},
   setup() {
     const bool = ref(false)
-    return {bool,Switch1Demo,Switch2Demo}
+    return {bool, Switch1Demo, Switch2Demo}
   }
 }
 </script>
