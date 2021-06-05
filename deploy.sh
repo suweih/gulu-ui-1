f7adc399374fae06d4f0a17d39a33bf731b1dc35
suweih <475464842@qq.com>
2021-06-05 23:21:39 +0800

    创建lib/index.ts

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:FrankFang/gulu-ui-website-1.git &&
+#git remote add origin git@gitee.com:suweih/gulu-ui-website.git &&
+git remote add origin git@github.com:suweih/gulu-ui-website.git &&
 git push -f -u origin master &&
 cd -
 echo https://suweih.github.io/gulu-ui-website/index.html#/
